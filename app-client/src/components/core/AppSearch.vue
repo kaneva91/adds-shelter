e11c990475224fbde43625422343b7a808b36264
Kalina Kaneva <kaneva91@gmail.com>
2020-04-11 21:41:38 +0300

    search styles

--- app-client/src/components/core/AppSearch.vue
+++ app-client/src/components/core/AppSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="search">
     <form @submit.prevent="searchHandler">
-      <input type="text" />
+      <input class="search-input" type="text" placeholder="Search..." />
 
       <select>
         <option value="null" selected>Category...</option>
@@ -11,7 +11,8 @@
         <option value="4">Option 4</option>
       </select>
 
-      <button class="search-btn">Search</button>
+      <!-- <button class="search-btn">Search  >></button> -->
+      <button class="search-btn"><span>Search </span></button>
     </form>
   </section>
 </template>
@@ -34,36 +35,82 @@ export default {
   margin: 20px auto;
   padding: 10px 10px;
   background: #111;
-}
-
-form {
-  margin: 0 auto;
-  font-size: 25px;
-  display: flex;
-  justify-content: space-between;
  
 }
 
-input[type="text"] {
-  width: 60%;
-  margin-left: 20px;
-  margin-right: 10px;
+form {
+  padding-top: 9px;
+  margin: 0 10px;
+  font-size: 25px;
+  display: flex;
+  justify-content: space-between;
 }
 
-select {
-  margin-top: 7.5px;
-  width: 20%;
+.search-input,
+select,
+.search-btn {
+  height: 25px;
+  width: 25%;
+  font-size: 16px;
+  outline: none;
 }
 
-input[type="text"],
+.search-input,
 select {
-  height: 30px;
+  background: #111;
+  border: none;
+  border-bottom: 1px solid white;
+  color: gray;
+  padding-left: 15px;
 }
+
 
 .search-btn {
-  width: 100px;
-  margin-right: 20px;
-  margin-left: 10px;
-  height: 30px;
+  line-height: 20px;
+  background-color: black;
+  border: 1px solid white;
+  line-height: 20px;
+  padding: 0;
+
+  border-radius: 5px;
+  transition: 0.5s;
+  height: 40px;
+  width:20%;
+  color: white;
+   margin-top: -9px;
+    transition: all 0.5s;
+     cursor: pointer;
 }
+
+.search-btn:active{
+   transform: translateY(4px);
+   color: black;
+   background-color: white;
+}
+
+.search-btn span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.search-btn span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.search-btn:hover span {
+  padding-right: 25px;
+}
+
+.search-btn:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
 </style>
